ea789a293be2e36d9bd507b36476b1dd870c82a6
Juan <juanestebanacostarodriguez@gmail.com>
2024-05-07 16:55:53 -0500

    Segundo commit
    Se añadio el express y funciona funciona el index.js en el puerto 3000

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-'use strict';
-const http = require('http');
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Hola Mundo');
-});
-server.listen(5000);
+const express = require('express')
+const app = express()
+const port =3000
+
+app.get('/', (req,res)=>{
+    res.send('Hola Mundo')
+})
+
+app.listen(port, () =>{
+    console.log('La aplicación se está ejecutando por el puerto ' + `${port}`);
+
+})

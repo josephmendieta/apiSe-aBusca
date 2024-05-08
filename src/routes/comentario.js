8f5a4683b99a3d360f68ffff039c3ade291d16ec
Katherine Michelle Ortiz <Katherineortiz4567@gmail.com>
2024-05-08 16:12:27 -0500

    Commit 20
    Se realizó el request de Get con su id de comentario.

--- src/routes/comentario.js
+++ src/routes/comentario.js
@@ -2,15 +2,22 @@ const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const comentarioSchema = require("../models/comentario");
 
-router.post("/comentarios", (req, res) => {
+router.post("/comentario", (req, res) => {
     const comentario = comentarioSchema(req.body);
     comentario
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-router.get("/comentarios", (req, res) => {
-    documentosSchema.find()
+router.get("/comentario", (req, res) => {
+    comentarioSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+router.get("/comentario/:id", (req, res) => {
+    const { id } = req.params;
+    comentarioSchema
+        .findById(id)
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });

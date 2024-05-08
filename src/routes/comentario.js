5242141f0bba4cd08b649753f7dc67035cf52ab3
OscarRamirez <oscarj.ramirezc@konradlorenz.edu.co>
2024-05-08 16:09:19 -0500

    Commit 19
    Se realizo el request de GET

--- src/routes/comentario.js
+++ src/routes/comentario.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const comentarioSchema = require("../models/comentario");
 
-router.post("/comentario", (req, res) => {
-    const comentario= comentarioSchema(req.body);
-     comentario
+router.post("/comentarios", (req, res) => {
+    const comentario = comentarioSchema(req.body);
+    comentario
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-
+router.get("/comentarios", (req, res) => {
+    documentosSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 module.exports = router;

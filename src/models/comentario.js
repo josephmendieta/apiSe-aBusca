9dcafa50beaef032fd98e7c8aca721118a9bdb03
Katherine Michelle Ortiz <Katherineortiz4567@gmail.com>
2024-05-08 15:58:10 -0500

    Commit 17
    Se realizó el modulo de comentario.

---
+++ src/models/comentario.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const comentarioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    correo: {
+        type: String,
+        required: true,
+    },
+    puntuacion: {
+        type: int,
+        required: true,
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    },
+    comentario: {
+        type: String,
+        required: true,
+    }
+    
+});
+module.exports = mongoose.model("Usuario", usuarioSchema);

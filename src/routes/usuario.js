f300c8e1b9aa3571cb2c329d6468090482f584d7
OscarRamirez <oscarj.ramirezc@konradlorenz.edu.co>
2024-05-08 15:11:12 -0500

    Commit 11
    Se realizo el request de POST

---
+++ src/routes/usuario.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const usuarioSchema = require("../models/usuario");
+//Nuevo animal
+router.post("/usuario", (req, res) => {
+    const usuario = usuarioSchema(req.body);
+    usuario
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+module.exports = router;

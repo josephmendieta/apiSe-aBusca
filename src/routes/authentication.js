dd5df4a513da9031f72ebd0631b1ac6fd87c50aa
KatherineOrtiz <katherinem.ortizb@konradlorenz.edu.co>
2024-05-09 15:42:09 -0500

    Commit 28
    Se realizó la petición Post para realizar el inicio de sesión.

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const usuarioSchema = require("../models/usuario");
+const bcrypt = require("bcrypt");
 router.post('/signup', async (req, res) => {
     const { nombre, correo, contrasena, fecharegistro, activo } = req.body;
     const usuario = new usuarioSchema({
@@ -15,6 +16,25 @@ router.post('/signup', async (req, res) => {
     //res.json(usuario);
     res.json({
         message: "Usuario guardado."
+    });
+});
+//inicio de sesión
+router.post("/login", async (req, res) => {
+    // validaciones
+    const { error } = usuarioSchema.validate(req.body.correo, req.body.contrasena);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    //Buscando el usuario por su dirección de correo
+    const usuario = await usuarioSchema.findOne({ correo: req.body.correo });
+    //validando si no se encuentra
+    if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+    //Transformando la contraseña a su valor original para 
+    //compararla con la clave que se ingresa en el inicio de sesión
+    const validPassword = await bcrypt.compare(req.body.contrasena, usuario.contrasena);
+    if (!validPassword)
+        return res.status(400).json({ error: "Contraseña no válida" });
+    res.json({
+        error: null,
+        data: "Bienvenido(a)",
     });
 });
 module.exports = router;

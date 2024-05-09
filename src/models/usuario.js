ba99102b4a051f72a013ab54a47cc78314bf4bf6
User <User@email.com>
2024-05-09 15:08:55 -0500

    Commit 24
    Se usó el bcrypt en el modulo de usuario.

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose"); // importando el componente mogoose
+const bcrypt =require ("bcrypt");
 const usuarioSchema = mongoose.Schema({
     nombre: {
         type: String,
@@ -20,6 +21,10 @@ const usuarioSchema = mongoose.Schema({
         type: String,
         required: true,
     }
-    
+   
 });
+usuarioSchema.methods.encryptContrasena = async(contrasena)=> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(contrasena, salt);
+}
 module.exports = mongoose.model("Usuario", usuarioSchema);
